5764aeb87d058a1c11de62094dfc3ec881b531f4
DumchevEvgeniy <souland@mail.ru>
2018-12-26 01:31:12 +0300

    add views

---
+++ RobotsGroupInteraction/Assets/ViewElements/FieldView.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.ViewElements {
+    public class FieldView : MonoBehaviour {
+        public Int32 Length = 30;
+        public Int32 Width = 20;
+        public Int32 NumberOfBarriers = 10;
+    }
+}
---
+++ RobotsGroupInteraction/Assets/ViewElements/MapViewAggregator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.ViewElements {
+    public class MapViewAggregator : MonoBehaviour {
+        public SmartMap Map { get; set; }
+
+        private void Start() {
+            var fieldView = GetComponent<FieldView>();
+            if (fieldView == null)
+                return;
+
+            var destinationPlaceView = GetComponent<DestinationPlaceView>();
+            if (destinationPlaceView == null)
+                return;
+
+            var robotViews = GetComponents<RobotView>();
+            
+            Map = new SmartMap(fieldView.Length, fieldView.Width, new MapFloor(), new ConcreteCube());
+            Map.AddElements<RandomPlacementWithPlayerDistance>(new BreakCube(), 10);
+            Map.CreateAll();
+        }
+    }
+}
